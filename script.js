87c421ba01e6fb1f69d6427ff0c14522f12ec1f5
Zals <afrizal.bagas0904@gmail.com>
2024-09-13 18:23:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar untuk menghilangkan menu
+const hamburger = document.querySelectorAll("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// up scroll button
+window.onscroll = function () {
+  let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+document.getElementById("scrollToTopBtn").onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
